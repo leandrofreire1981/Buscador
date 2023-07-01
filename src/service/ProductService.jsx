c6bbea184923c2f9907ecc0b6142d8f1d9bffd7e
leandrofreire1981 <freire.leandro@hotmail.com>
2023-07-01 17:12:49 -0300

    2

--- src/service/ProductService.jsx
+++ src/service/ProductService.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
-import dotenv from 'dotenv'
+//import dotenv from 'dotenv'
 
-dotenv.config()
+//dotenv.config()
 
 const API_URL = process.env.REACT_APP_API_URL; // Cambia esta URL según la configuración de tu API
 /* const API_URL = {data :[{
